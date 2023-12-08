7787c679d43008356388e2cdf9d39c2948b31fef
kouamschekina <kouamdelphine82@gmail.com>
2023-12-08 16:41:09 +0100

    fix(main):explanation of the code using comments

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from src.apriori import apriori
 
 if __name__ == '__main__':
-    result = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
-    print(result)
+    #assigning the value of apriori to the results variable
+    results = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
+    #display results
+    print(results)

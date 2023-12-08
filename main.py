9f9256c6bf24bac31b45bca326d66b97f1d9b724
kouamschekina <kouamdelphine82@gmail.com>
2023-12-08 17:16:17 +0100

    fix(main):explanation of the code using comments

--- main.py
+++ main.py
@@ -1,7 +1,17 @@
 from src.apriori import apriori
+def multiplication():
+    try:
+        a = input(float("enter the first number"))
+        b = input(float("enter the second number"))
+        product = (a*b)
+        return product
+    except ValueError:
+        return("enter a valid number")
+    
 
 if __name__ == '__main__':
     #assigning the value of apriori to the results variable
-    results = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
-    #display results
-    print(results)
+    result = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
+    #display result
+    print(result)
+multiplication()

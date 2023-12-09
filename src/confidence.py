576cb1a9ba52b037e0082bcadae40e1cbdb8d160
AssahBismarkabah <assah9799@gmail.com>
2023-12-09 12:25:22 +0100

    added tests to notebook file

--- src/confidence.py
+++ src/confidence.py
@@ -1,25 +1,20 @@
-dataset_type = list[list[object]]
-rule_type = tuple[list[object], list[object]]
+from typing import List, Tuple
 from support import support
 
-def confidence(data_set: dataset_type, rule: rule_type) -> float:
+dataset_type = List[List[object]]
+rule_type = Tuple[List[object], List[object]]
+
+
+def confidence(dataset: dataset_type, rule: rule_type) -> float:
     """
     To measure the likelihood of occurrence of an itemset given another itemset.
     """
-    #association rule in the form (antecedent, consequent).
     antecedent, consequent = rule
+    antecedent_support = support(antecedent, dataset)
+    rule_support = support(antecedent + consequent, dataset)
 
-    support_ab = support(antecedent + consequent, data_set)
+    if antecedent_support == 0:
+        return 0  # Avoid division by zero
 
-    support_a = support(antecedent, data_set)
-
-    if support_a != 0:
- #validating the value of the Supp of the antecedent before computation
- 
-        confidence_value = support_ab / support_a
-
-    else:
-        confidence_value = 0
-
+    confidence_value = rule_support / antecedent_support
     return confidence_value
-    

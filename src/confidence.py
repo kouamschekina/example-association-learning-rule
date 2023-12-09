3038e5c40159f414d12b3076dc41086572cccb91
kouamschekina <kouamdelphine82@gmail.com>
2023-12-09 12:05:45 +0100

    feat(global): Implementation of the support and confidence scripts

--- src/confidence.py
+++ src/confidence.py
@@ -1,9 +1,18 @@
+import support
 dataset_type = list[list[object]]
 rule_type = tuple[list[object], list[object]]
 
 
 def confidence(data_set: dataset_type, rule: rule_type) -> float:
+
+    support_AB = support(data_set, rule[0] + rule[1])
+    support_A = support(data_set, rule[0])
+    if support_A != 0 :
+        confidence = support_AB / support_A
+    else:
+        confidence = 0
+    return confidence
     """
     To measure the likelihood of occurrence of an itemset given another itemset.
     """
-    pass
+pass
--- src/support.py
+++ src/support.py
@@ -3,6 +3,14 @@ dataset_type = list[list[object]]
 
 
 def support(itemsets: dataset_type, data_set: itemset_type) -> float:
+    count = 0
+    for transaction in itemsets:
+        total_transactions = len(itemsets)
+        if set(data_set).issubset(transaction):
+            count += 1
+        support = count / total_transactions
+        return support
+
     """
     To find the frequency of itemsets in the dataset.
     """

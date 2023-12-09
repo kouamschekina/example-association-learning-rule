ea6bcfa2daa4babebb0f657d30a242a92214d415
AssahBismarkabah <assah9799@gmail.com>
2023-12-09 05:13:17 +0100

    Implement confidence and support calculation functions

--- src/confidence.py
+++ src/confidence.py
@@ -1,9 +1,25 @@
 dataset_type = list[list[object]]
 rule_type = tuple[list[object], list[object]]
-
+from support import support
 
 def confidence(data_set: dataset_type, rule: rule_type) -> float:
     """
     To measure the likelihood of occurrence of an itemset given another itemset.
     """
-    pass
+    #association rule in the form (antecedent, consequent).
+    antecedent, consequent = rule
+
+    support_ab = support(antecedent + consequent, data_set)
+
+    support_a = support(antecedent, data_set)
+
+    if support_a != 0:
+ #validating the value of the Supp of the antecedent before computation
+ 
+        confidence_value = support_ab / support_a
+
+    else:
+        confidence_value = 0
+
+    return confidence_value
+    
--- src/support.py
+++ src/support.py
@@ -6,4 +6,21 @@ def support(itemsets: dataset_type, data_set: itemset_type) -> float:
     """
     To find the frequency of itemsets in the dataset.
     """
-    pass
+    #initialising the counter to keep track of the occurance in the list
+    count=0
+
+    total_transactions=len(data_set)
+
+#looping through the entire dataset for occurance of itemset
+
+    for transaction in data_set:
+        if set(itemsets).issubset(transaction):
+
+            count = count + 1
+ #implementing support method
+ 
+    support_value=count / total_transactions
+
+    return support_value
+
+    

2f8c474e298733b8b2665ff62eb0b912408e238e
AssahBismarkabah <assah9799@gmail.com>
2023-12-09 13:41:31 +0100

    revert accidental changes in the --> main

--- main.py
+++ main.py
@@ -1,22 +1,5 @@
 from src.apriori import apriori
 
-dataset = [
-    [1, 2, 3],
-    [1, 2, 3],
-    [1, 2, 3],
-]
-
-min_support = 0.3
-min_confidence = 0.7
-
-# Call the apriori function
-frequent_itemsets, strong_rules = apriori(dataset, min_support, min_confidence)
-
-# Display the results
-print("Frequent Itemsets:")
-for itemset, support_value in frequent_itemsets.items():
-    print(f"{itemset}: {support_value}")
-
-print("\nStrong Rules:")
-for rule, confidence_value in strong_rules.items():
-    print(f"{rule[0]} -> {rule[1]} (Confidence: {confidence_value})")
+if __name__ == '__main__':
+    result = apriori([[1, 2, 3], [1, 2, 3], [1, 2, 3]], 0.3, 0.7)
+    print(result)
